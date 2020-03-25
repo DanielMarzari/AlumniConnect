c7c9d7f8e710dc833b11b0ac5eaea435a9363ac6
Daniel Marzari <Dan.marzari@gmail.com>
2020-03-25 17:40:48 -0400

    Update AlumniConnect.sql

--- Database/AlumniConnect.sql
+++ Database/AlumniConnect.sql
@@ -13,5 +13,36 @@ CREATE TABLE Events (
 	Invitees VARCHAR(100) NOT NULL,
 	Location TEXT NOT NULL,
 	Name VARCHAR(255) NOT NULL
+) ENGINE MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE Users (
+	Username VARCHAR(100) NOT NULL PRIMARY KEY,
+	Password VARCHAR(100) NOT NULL, 	
+	Permissions VARCHAR(20) NOT NULL
 ) ENGINE InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE Alumni (
+	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	FullName VARCHAR(255) NOT NULL,
+	Phone VARCHAR(30), -- this should avoid all odd formatting errors and extensions
+	Email VARCHAR(100),
+	Website VARCHAR(255),
+	LinkedInURL VARCHAR(255),
+	Bio VARCHAR(255) NOT NULL,
+	Friends MEDIUMTEXT, -- as array of ID e.g. {1, 14, 19}
+	PictureURL VARCHAR(255), --URL link to picture
+	User_ID VARCHAR(100) NOT NULL,
+	FOREIGN KEY (User_ID) REFERENCES Users(Username)
+) ENGINE InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE WorkHistory (
+	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	Company VARCHAR(255) NOT NULL,
+	Title VARCHAR(100) NOT NULL,
+	Part_FullTime VARCHAR(20) NOT NULL,
+	StartDate DATE NOT NULL,
+	EndDate DATE, -- can be NULL if present position
+	Description VARCHAR(255) NOT NULL,
+	Alumni_ID INT,
+	FOREIGN KEY (Alumni_ID) REFERENCES Alumni(ID)
+) ENGINE InnoDB DEFAULT CHARSET=latin1;
